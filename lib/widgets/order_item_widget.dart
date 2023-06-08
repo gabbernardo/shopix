0ad710d313eff76eb01b0f9a7464bf77d5ffa70d
gabbernardo <gaab.bernardo00@gmail.com>
2023-06-08 12:39:01 +0800

    feat: animation for order screen

--- lib/widgets/order_item_widget.dart
+++ lib/widgets/order_item_widget.dart
@@ -18,47 +18,51 @@ class _OrderItemWidgetState extends State<OrderItemWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10.0),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('Total: ₱ ${widget.orderItem.totalAmount.toStringAsFixed(2)}'),
-            subtitle: Text(
-                'Date: ${DateFormat('dd/MM/yyyy hh:mm').format(widget.orderItem.dateTime)}'),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
-          ),
-          if (_expanded)
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 4.0),
-              height: min(widget.orderItem.products.length * 20 + 10, 100),
-              child: ListView.builder(
-                itemCount: widget.orderItem.products.length,
-                itemBuilder: (ctx, index) => Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      widget.orderItem.products[index].title,
-                      style: const TextStyle(
-                          fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      '${widget.orderItem.products[index].quantity}x ₱ ${widget.orderItem.products[index].price} ',
-                      style: const TextStyle(
-                          fontSize: 18, color: Colors.grey),
-                    ),
-                  ],
-                ),
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: _expanded ? min(widget.orderItem.products.length * 20 + 110, 200) : 95,
+      child: Card(
+        margin: EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('Total: ₱ ${widget.orderItem.totalAmount.toStringAsFixed(2)}'),
+              subtitle: Text(
+                  'Date: ${DateFormat('dd/MM/yyyy hh:mm').format(widget.orderItem.dateTime)}'),
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
               ),
             ),
-        ],
+              AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 4.0),
+                height: _expanded ? min(widget.orderItem.products.length * 20 + 10, 100) : 0,
+                child: ListView.builder(
+                  itemCount: widget.orderItem.products.length,
+                  itemBuilder: (ctx, index) => Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        widget.orderItem.products[index].title,
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '${widget.orderItem.products[index].quantity}x ₱ ${widget.orderItem.products[index].price} ',
+                        style: const TextStyle(
+                            fontSize: 18, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
